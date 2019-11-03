3b41603cb75ef3a577a7dbc9dceaaa94c6d3b00c
Seth Giovanetti <gio@utdallas.edu>
2019-11-03 16:56:28 -0600

    Test for outdated python versions and warn users.

--- src/patch_and_run.py
+++ src/patch_and_run.py
@@ -1,5 +1,17 @@
 #!/bin/python3
 
+from sys import version_info
+
+try:
+    assert version_info.major >= 3
+    assert version_info.minor >= 6
+except (AssertionError, AttributeError):
+    print("This script requires Python 3.6 or newer.")
+    print("Press enter to exit.")
+    input()
+    exit()
+
+
 import subprocess
 import os
 import glob
