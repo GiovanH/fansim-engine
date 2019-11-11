57ef60b1fd25f74f8c2c7933bdcc4ac455b351a5
Seth Giovanetti <gio@utdallas.edu>
2019-11-11 16:51:39 -0600

    Use readme.md for github

--- doc/make.sh
+++ doc/make.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rm catalog.md
+rm readme.md
 
 for file in $(ls -rt *.png *.gif *.jfif)
 do
     modtime=$(stat -c %y pq-ms-2.gif | cut -d '.' -f1)
     echo $file $modtime
-    echo "| ![${file}](${file})             |" >> catalog.md
-    echo "| ------------------------------- |" >> catalog.md
-    echo "| [${file}](${file}) (${modtime}) |" >> catalog.md
-    echo "" >> catalog.md
+    echo "| ![${file}](${file})             |" >> readme.md
+    echo "| ------------------------------- |" >> readme.md
+    echo "| [${file}](${file}) (${modtime}) |" >> readme.md
+    echo "" >> readme.md
 done
