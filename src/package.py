1e8a96d7ed01a231951d0efc2ab99774fedfa8d0
Seth Giovanetti <gio@utdallas.edu>
2019-11-21 21:45:19 +0000

    Bugfixes

--- src/package.py
+++ src/package.py
@@ -36,11 +36,14 @@ for package_id in sys.argv[1:]:
     with open(os.path.join(pysubdir_new, f"{package_id}.rpy"), "w") as fp:
         fp.write(merged)
 
-    cmdstr = f"python rpatool.py -c ../custom_volumes/{package_id}_min/assets.rpa"
+    rpapath = f"../custom_volumes/{package_id}_min/assets.rpa"
+    cmdstr = f"python rpatool.py -c {rpapath}"
     if os.path.isdir(os.path.join(pysubdir, "assets")):
         cmdstr += f" custom_assets_{package_id}={pysubdir}assets"
     if os.path.isdir(os.path.join(pysubdir, "assets_common")):
         cmdstr += f" custom_assets={pysubdir}assets_common"
     print(cmdstr)
-    os.unlink(f"../custom_volumes/{package_id}_min/assets.rpa")
+
+    if os.path.isfile(rpapath):
+        os.unlink(rpapath)
     retcode = subprocess.call(cmdstr.split(" "))
