2236761158e47178a4c102d66df4c4e55588693e
Gio <sethg@ipi.org>
2020-01-09 19:05:00 -0600

    Rearrange scripts

---
+++ src/scripts/package_rpa.py
@@ -0,0 +1,53 @@
+# Experimental
+# RPA files go in custom_volumes/{package_id}/
+# You should not use RPA files if you also have an assets folder!
+
+
+from sys import version_info
+
+try:
+    assert version_info.major >= 3
+    assert version_info.minor >= 6
+except (AssertionError, AttributeError):
+    print("This script requires Python 3.6 or newer.")
+    print("Press enter to exit.")
+    input()
+    exit()
+
+import os
+import sys
+import subprocess
+import glob
+import shutil
+
+fse_root = "../.."
+rpatool = "../rpatool.py"
+
+for package_id in sys.argv[1:]:
+    pysubdir = os.path.join(fse_root, f"custom_volumes_other/{package_id}/")
+
+    pysubdir_new = os.path.join(fse_root, f"custom_volumes/{package_id}_min/")
+
+    os.makedirs(pysubdir_new, exist_ok=True)
+
+    shutil.copy2(f"{pysubdir}meta.json", f"{pysubdir_new}meta.json")
+    shutil.copy2(f"{pysubdir}credits.yml", f"{pysubdir_new}credits.yml")
+
+    merged = "init offset = 1\n\n"
+    for rpy in glob.glob(os.path.join(pysubdir, "*.rpy")):
+        with open(rpy, "r") as rpyfp:
+            merged += f"# {rpy}\n{rpyfp.read()}\n"
+    with open(os.path.join(pysubdir_new, f"{package_id}.rpy"), "w") as fp:
+        fp.write(merged)
+
+    rpapath = os.path.join(fse_root, f"custom_volumes/{package_id}_min/assets.rpa")
+    cmdstr = f"python {rpatool} -c {rpapath}"
+    if os.path.isdir(os.path.join(pysubdir, "assets")):
+        cmdstr += f" custom_assets_{package_id}={pysubdir}assets"
+    if os.path.isdir(os.path.join(pysubdir, "assets_common")):
+        cmdstr += f" custom_assets={pysubdir}assets_common"
+    print(cmdstr)
+
+    if os.path.isfile(rpapath):
+        os.unlink(rpapath)
+    retcode = subprocess.call(cmdstr.split(" "))
