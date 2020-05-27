0074dff63e227e5556af0a232ccb7b76b0821502
Gio <sethg@ipi.org>
2020-05-27 15:43:47 -0500

    Freshjamz button improvements

--- src/sys/assets/freshjamz/make.sh
+++ src/sys/assets/freshjamz/make.sh
@@ -1,6 +1,7 @@
 for s in play pause prev next single shuffle
 do
     magick composite ${s}.png button_idle.png -geometry +0+0  ${s}_idle.png
+    magick composite ${s}.png button_hover.png -geometry +0+0  ${s}_hover.png
     magick composite ${s}.png button_selected_idle.png -geometry +7+5  ${s}_selected_idle.png
     magick composite ${s}.png button_selected_hover.png -geometry +7+5  ${s}_selected_hover.png
 done
